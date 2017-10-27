3f8561e17d3758bfc38dba6505bd40e03c9ef669
tylermiller94 <tylermiller94@gmail.com>
2017-10-27 14:13:59 -0700

    span outputs for animals

--- app/app.component.ts
+++ app/app.component.ts
@@ -17,11 +17,32 @@ import { Component } from '@angular/core';
        <span>Likes:</span>
        <span>Dislikes:</span>
      </div>
+     <div *ngFor="let currentAnimal of animals">
+       <span>{{species}}</span>
+       <span>{{name}}</span>
+       <span>{{age}}</span>
+       <span>{{diet}}</span>
+       <span>{{location}}</span>
+       <span>{{caretakers}}</span>
+       <span>{{sex:}}</span>
+       <span>{{likes}}</span>
+       <span>{{dislikes}}</span>
     </div>
   </div>
   `
 })
 
 export class AppComponent {
+  zooName: string = "Local Zoo";
+  animals: Animal[] = [
+    new Animal("Arctic Fox", "Moon", 2, "Carnivore", "Northern Trail", 5, "Female", "Cool sahde", "Loud Noises" ),
+    new Animal("Ocelot", "Prince", 4, "Carnivore", "Tropical Rain Forest Building", 6, "Male", "Lying in the sunshine", "Toys that are not rope-based"),
+    new Animal("Northwest Black Tailed Deer", "Tinkerbell", 8, "Herbivore", "Northern Trail", 2, "Female", "Delicate roots and leaves", "Loud Noises"),
+
+  ];
+
+
+
+
 
 }
