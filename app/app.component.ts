5a661892c7199ea9a36e8ff7307ec02e3e2da5a5
tylermiller94 <tylermiller94@gmail.com>
2017-10-27 12:04:36 -0700

    add spans for animal information

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>Angular2 Scaffold</h1>
+  <h1>local zoo</h1>
+  <div class="container">
+   <div class="animals">
+     <div class="title">
+       <span>species:</span>
+       <span>name</span>
+       <span>Age:</span>
+       <span>Diet:</span>
+       <span>Location</span>
+       <span>Caretakers:</span>
+       <span>Sex:</span>
+       <span>Likes:</span>
+       <span>Dislikes:</span>
+     </div>
+    </div>
+  </div>
   `
 })
 
